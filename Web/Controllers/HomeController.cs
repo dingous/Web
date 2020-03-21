ed9203640e6ea22f7dfadc69e716d04a0a00d7e4
Diego Salinas <codigosetutoriais@gmail.com>
2020-03-21 18:07:05 -0300

    Primeiro versionamento

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+
+			var config = new ConfigurationBuilder()
+			  .SetBasePath(Directory.GetCurrentDirectory())
+			  .AddJsonFile("appsettings.json").Build();
+
+			//var host = HttpContext.Connection.RemoteIpAddress.ToString();
+			var host = "localhost";
+			host = host.Replace(":", "").Replace("f", "");
+
+			string UrlApi1 = config["UrlApi1"].Replace("[host]", host);
+			string UrlApi2 = config["UrlApi2"].Replace("[host]", host);
+
+
+
+			ViewBag.UrlApi1 = UrlApi1;
+			ViewBag.UrlApi2 = UrlApi2;
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Web/wwwroot/js/site.js
@@ -0,0 +1,118 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function BuscaTaxaJuros(url) {
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		cache: false,
+		beforeSend: function (data) {
+
+			$("#result1").html("...");
+		},
+		success: function (data) {
+
+			$("#result1").html(data);
+
+		},
+		error: function (jqXHR, exception) {
+
+			$("#result1").html(exception);
+		}
+	});
+
+}
+
+
+function CalculaJuros(url) {
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		cache: false,
+		beforeSend: function (data) {
+
+			$("#result2").html("...");
+		},
+		success: function (data) {
+
+			$("#result2").html(data);
+
+
+		},
+		error: function (jqXHR, exception) {
+
+			$("#result2").html(exception);
+		}
+	});
+
+}
+
+
+
+function BuscaRepositorios(url) {
+
+	$.ajax({
+		type: "GET",
+		url: url,
+		cache: false,
+		beforeSend: function (data) {
+
+			$("#tabelaGit").hide();
+			$("#result3").html("...");
+		},
+		success: function (data) {
+
+			let html = "";
+			let contador = 1;
+			$(data).each(function (index, element) {
+
+				html += "<tr><th scope='row'>" + contador + "</th><td>" + $(this)[0].repositorio + "</td><td><a class='btn btn-secondary' href='" + $(this)[0].url + "' target='_blank' role='button'>" + $(this)[0].url + "</a></td></tr>";
+				contador++;
+
+			});
+
+			$("#result3").html(html);
+
+			$("#tabelaGit").show();
+
+		},
+		error: function (jqXHR, exception) {
+
+			$("#result3").html(exception);
+		}
+	});
+
+}
+
+$(document).ready(function () {
+
+	$("#btnConsultaTaxaJuros").click(function (arg, e) {
+
+		let Url = $("#UrlApi1").val() + "/taxaJuros";
+
+		BuscaTaxaJuros(Url);
+
+	});
+
+	$("#btnTaxaJuros").click(function (arg, e) {
+
+		let Url = $("#UrlApi2").val() + "/calculajuros/?valorinicial=" + $("#ValorInicial").val() + "&meses=" + $("#Meses").val();
+
+		CalculaJuros(Url);
+
+	});
+
+
+	$("#btnShowmecode").click(function (arg, e) {
+
+		let Url = $("#UrlApi2").val() + "/showmethecode";
+
+		BuscaRepositorios(Url);
+
+	});
+});
